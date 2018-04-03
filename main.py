b12560eb6ab739978e3d4c438ceaad62bec1cb18
mizuff <flipflop.develop@gmail.com>
2018-04-04 00:10:14 +0900

    on_voice_state_update()の動作をbefore, afterいずれかがNoneのときに限定

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ CHANNEL_ID = config.get('DISCORD', 'CHANNEL_ID')
 
 class MemberLog:
     def __init__(self, member):
-        self._member = member
+        self.member = member
         self.join_time = None
 
     def set_join_time(self):
@@ -32,18 +32,24 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
+    # member_log = []
+
     for member in client.get_server(SERVER_ID).members:
         member_log = MemberLog(member)
         member_before = member_after = member
 
         @client.event
         async def on_voice_state_update(member_before, member_after):
-            if member_after.voice.voice_channel and member_after.voice.voice_channel.id == CHANNEL_ID:
+        # on mutedでもこの関数は呼ばれるので, 動作をbefore, afterいずれかがNoneのときに限定
+            if not member_before.voice.voice_channel and member_after.voice.voice_channel:
                 member_log.set_join_time()
-            elif member_before.voice.voice_channel and member_before.voice.voice_channel.id == CHANNEL_ID:
+            elif member_before.voice.voice_channel and not member_after.voice.voice_channel:
                 if member_log.join_time:    # 起動時に既にjoinしていた場合は除外
-                    diff_second = time.time() - member_log.join_time
-                    # message = '{} has connected to {} for {}.'.format(member_log)
-                    print(second_to_hour(diff_second))
+                    diff_float = time.time() - member_log.join_time
+                    diff_str = second_to_hour(diff_float)
+                    message = '{} has connected to {} for {}.'.format(
+                        member_log.member.name, client.get_channel(CHANNEL_ID).name, diff_str
+                    )
+                    print(message)
 
 client.run(TOKEN)
