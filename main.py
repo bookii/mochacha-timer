2014f99dac15ad34e125a987f1a1fb56036c55bf
mizuff <flipflop.develop@gmail.com>
2018-04-03 23:49:36 +0900

    MemberLogクラスのgetterの削除

--- main.py
+++ main.py
@@ -12,14 +12,11 @@ CHANNEL_ID = config.get('DISCORD', 'CHANNEL_ID')
 
 class MemberLog:
     def __init__(self, member):
-        self.member = member
+        self._member = member
         self.join_time = None
 
     def set_join_time(self):
         self.join_time = time.time()
-    
-    def get_join_time(self):
-        return self.join_time
 
 def second_to_hour(second):
     hour = second // 3600
@@ -43,9 +40,10 @@ async def on_ready():
         async def on_voice_state_update(member_before, member_after):
             if member_after.voice.voice_channel and member_after.voice.voice_channel.id == CHANNEL_ID:
                 member_log.set_join_time()
-            elif member_before.voice.voice_channel and member_before.voice.voice_channel.id:
-                if member_log.get_join_time():    # 起動時に既にjoinしていた場合は除外
-                    diff_second = time.time() - member_log.get_join_time()
+            elif member_before.voice.voice_channel and member_before.voice.voice_channel.id == CHANNEL_ID:
+                if member_log.join_time:    # 起動時に既にjoinしていた場合は除外
+                    diff_second = time.time() - member_log.join_time
+                    # message = '{} has connected to {} for {}.'.format(member_log)
                     print(second_to_hour(diff_second))
 
 client.run(TOKEN)
